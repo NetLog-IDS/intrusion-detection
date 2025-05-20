dee857707b4c87b79283d8417c129650f3758c44
Emyr298 <emi.ru298@gmail.com>
2025-05-20 08:25:03 +0700

    feat: deploy new model

--- ai-detection/deployment/predictions/dos.py
+++ ai-detection/deployment/predictions/dos.py
@@ -5,7 +5,7 @@ import numpy as np
 from .predictor import Predictor
 
 class DoSPredictor(Predictor):
-    SELECTED_FEATURES = ['Bwd Packet Length Std', 'Average Packet Size', 'Bwd Segment Size Avg', 'Packet Length Std', 'Bwd Packet Length Mean', 'FWD Init Win Bytes', 'Bwd Init Win Bytes', 'Packet Length Variance', 'Bwd Packet Length Max', 'Packet Length Max', 'Total Length of Bwd Packet', 'Packet Length Mean', 'Fwd Seg Size Min', 'Fwd RST Flags', 'Idle Min', 'Bwd PSH Flags', 'Fwd IAT Min', 'Flow IAT Min', 'Total Connection Flow Time', 'Idle Std', 'Fwd IAT Max', 'Bwd Bulk Rate Avg', 'FIN Flag Count', 'RST Flag Count', 'Total Bwd packets', 'Total Fwd Packet', 'Protocol', 'Fwd Packet Length Std', 'Down/Up Ratio', 'Bwd IAT Total', 'Active Min', 'Flow IAT Max', 'SYN Flag Count', 'Fwd Packet Length Max', 'Active Mean', 'Flow Duration', 'Fwd IAT Total', 'Idle Max', 'Bwd Packets/s', 'Bwd Act Data Pkts', 'Fwd PSH Flags', 'Bwd IAT Max', 'Idle Mean', 'Fwd Header Length', 'Bwd IAT Min']
+    SELECTED_FEATURES = ['Fwd IAT Min', 'Bwd IAT Mean', 'Fwd Seg Size Min', 'Flow Packets/s', 'Fwd Packets/s', 'Total Length of Bwd Packet', 'Bwd Act Data Pkts', 'Bwd IAT Min', 'Flow IAT Mean', 'FIN Flag Count', 'Bwd Init Win Bytes', 'Fwd Bulk Rate Avg', 'Active Min', 'Fwd Packet Length Max', 'Fwd Packet Length Min', 'Bwd Packets/s', 'Bwd Header Length', 'Bwd Packet Length Mean', 'Subflow Bwd Bytes', 'Flow Duration', 'Flow IAT Min', 'Total Bwd packets', 'Fwd Segment Size Avg', 'FWD Init Win Bytes', 'Packet Length Mean', 'Down/Up Ratio', 'ACK Flag Count', 'Fwd IAT Total', 'SYN Flag Count', 'Bwd Packet Length Max', 'Subflow Fwd Packets', 'Fwd Bytes/Bulk Avg', 'Flow IAT Max', 'Fwd IAT Mean', 'Fwd IAT Std', 'Fwd Header Length']
     FEATURE_TO_JSON_FIELD = {v: k for k, v in Predictor.JSON_FIELD_TO_FEATURE.items()}
 
     def __init__(self):
@@ -17,10 +17,10 @@ class DoSPredictor(Predictor):
         warnings.filterwarnings("ignore")
 
         try:
-            self.stack = joblib.load("models/dos/stk3.pkl")
-            self.rf = joblib.load("models/dos/rf.pkl")
-            self.et = joblib.load("models/dos/et.pkl")
-            self.lgbm = joblib.load("models/dos/lgbm.pkl")
+            self.stack = joblib.load("models/dos/stk3_tuned.pkl")
+            self.rf = joblib.load("models/dos/rf_tuned.pkl")
+            self.xg = joblib.load("models/dos/xg_tuned.pkl")
+            self.lgbm = joblib.load("models/dos/lgbm_tuned.pkl")
         except Exception as e:
             print("Invalid Model")
             raise
@@ -36,17 +36,16 @@ class DoSPredictor(Predictor):
         rf_test=self.rf.predict(prepared_rows).reshape(-1, 1)
         rf_prob_test=self.rf.predict_proba(prepared_rows)
 
-        et_test=self.et.predict(prepared_rows).reshape(-1, 1)
-        et_prob_test=self.et.predict_proba(prepared_rows)
+        xg_test=self.xg.predict(prepared_rows).reshape(-1, 1)
+        xg_prob_test=self.xg.predict_proba(prepared_rows)
 
         lgbm_test=self.lgbm.predict(prepared_rows).reshape(-1, 1)
         lgbm_prob_test=self.lgbm.predict_proba(prepared_rows)
 
-        top_3_test_predictions = [rf_test, et_test, lgbm_test]
-        top_3_test_proba = [rf_prob_test, et_prob_test, lgbm_prob_test]
+        top_3_test_predictions = [rf_test, xg_test, lgbm_test]
+        top_3_test_proba = [rf_prob_test, xg_prob_test, lgbm_prob_test]
 
         x_test = np.concatenate(top_3_test_predictions + top_3_test_proba, axis=1)
         y_predict=self.stack.predict(x_test)
 
-        # Note that in DoS model, 0 = DoS and 1 = Not DoS
-        return [(False if pred else True) for pred in y_predict]
+        return [(True if pred else False) for pred in y_predict]
--- ai-detection/deployment/predictions/port_scan.py
+++ ai-detection/deployment/predictions/port_scan.py
@@ -5,7 +5,7 @@ import numpy as np
 from .predictor import Predictor
 
 class PortScanPredictor(Predictor):
-    SELECTED_FEATURES = ['Total Connection Flow Time', 'Fwd Seg Size Min', 'Bwd RST Flags', 'Fwd Packet Length Max', 'Flow Duration', 'Packet Length Mean', 'Flow IAT Max', 'Fwd Packet Length Mean', 'Average Packet Size', 'Bwd IAT Total', 'Total Length of Bwd Packet', 'Bwd Packets/s', 'Total Length of Fwd Packet', 'Fwd Packets/s', 'Fwd Act Data Pkts', 'Flow Bytes/s', 'Bwd Packet Length Max', 'Bwd Segment Size Avg', 'Flow IAT Min', 'Packet Length Max', 'Fwd Segment Size Avg']
+    SELECTED_FEATURES = ['Bwd RST Flags', 'FWD Init Win Bytes', 'RST Flag Count', 'Flow Duration', 'Packet Length Max', 'Flow Packets/s', 'Protocol', 'Total Length of Fwd Packet', 'Flow IAT Max', 'Fwd Seg Size Min', 'Average Packet Size', 'Packet Length Mean', 'Fwd Packet Length Max', 'Bwd IAT Mean', 'Total Fwd Packet', 'Flow Bytes/s', 'Fwd Act Data Pkts', 'Bwd Packets/s', 'Bwd Packet Length Std', 'Fwd IAT Total', 'Flow IAT Mean', 'Flow IAT Min', 'Fwd Segment Size Avg', 'Fwd Header Length', 'Bwd Packet Length Mean', 'Packet Length Std', 'Fwd IAT Min', 'Packet Length Variance', 'Bwd IAT Max']
     FEATURE_TO_JSON_FIELD = {v: k for k, v in Predictor.JSON_FIELD_TO_FEATURE.items()}
 
     def __init__(self):
@@ -17,10 +17,10 @@ class PortScanPredictor(Predictor):
         warnings.filterwarnings("ignore")
 
         try:
-            self.stack = joblib.load("models/port_scan/stk3.pkl")
-            self.xg = joblib.load("models/port_scan/xg.pkl")
-            self.rf = joblib.load("models/port_scan/rf.pkl")
-            self.lgbm = joblib.load("models/port_scan/lgbm.pkl")
+            self.stack = joblib.load("models/port_scan/stk3_tuned.pkl")
+            self.et = joblib.load("models/port_scan/et_tuned.pkl")
+            self.rf = joblib.load("models/port_scan/rf_tuned.pkl")
+            self.lgbm = joblib.load("models/port_scan/lgbm_tuned.pkl")
         except Exception as e:
             print("Invalid Model")
             raise
@@ -33,8 +33,8 @@ class PortScanPredictor(Predictor):
                 prepared_row[feature] = row[json_field]
             prepared_rows.append(list(prepared_row.values()))
         
-        xg_test=self.xg.predict(prepared_rows).reshape(-1, 1)
-        xg_prob_test=self.xg.predict_proba(prepared_rows)
+        et_test=self.et.predict(prepared_rows).reshape(-1, 1)
+        et_prob_test=self.et.predict_proba(prepared_rows)
 
         rf_test=self.rf.predict(prepared_rows).reshape(-1, 1)
         rf_prob_test=self.rf.predict_proba(prepared_rows)
@@ -42,8 +42,8 @@ class PortScanPredictor(Predictor):
         lgbm_test=self.lgbm.predict(prepared_rows).reshape(-1, 1)
         lgbm_prob_test=self.lgbm.predict_proba(prepared_rows)
 
-        top_3_test_predictions = [xg_test, rf_test, lgbm_test]
-        top_3_test_proba = [xg_prob_test, rf_prob_test, lgbm_prob_test]
+        top_3_test_predictions = [et_test, rf_test, lgbm_test]
+        top_3_test_proba = [et_prob_test, rf_prob_test, lgbm_prob_test]
 
         x_test = np.concatenate(top_3_test_predictions + top_3_test_proba, axis=1)
         y_predict=self.stack.predict(x_test)
