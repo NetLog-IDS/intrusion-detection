d907c0fd774c5817188830d49881a8a6e5dd90c3
Emyr298 <emi.ru298@gmail.com>
2025-05-25 12:56:23 +0700

    feat: more evaluation

--- rule-detection/evaluation/consumer/script.py
+++ rule-detection/evaluation/consumer/script.py
@@ -42,7 +42,7 @@ def get_json_messages(bootstrap_servers, topic, group_id='python-json-consumer')
         finally:
             consumer.close()
         
-        with open("result_2_portscan.json", "w") as file:
+        with open("flink_portscan.json", "w") as file:
             file.write(json.dumps(result_list, indent=2))
         
             
